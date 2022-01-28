e221ec1c888c142c482001c3c8903515e64ea711
IskahnX <davidecroce10@gmail.com>
2022-01-28 16:01:53 +0100

    added list group and array in the data

--- js/script.js
+++ js/script.js
@@ -2,3 +2,23 @@ console.log('VUE is OK', Vue);
 
 Vue.config.devtools = true;
 
+const goodVibes = new Vue ({
+    el: '#root',
+    data:{
+        things: [
+            {
+                text: 'Disegnare più spesso',
+                done: false,
+            },
+            {
+                text: 'Ritornare in palestra',
+                done: false,
+            },
+            {
+                text: 'Giocare il meno possibilie al pc',
+                done: false,
+            }
+        ]
+    },
+        
+})
